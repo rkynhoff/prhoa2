edc1b676c9301b7e8cf76fe38b1077c7ee923c1a
Rachel Kynhoff <rkynhoff@gmail.com>
2019-07-15 20:00:36 -0400

    new html and JS

--- scriptV2.js
+++ scriptV2.js
@@ -17,3 +17,31 @@ window.onclick = function(event) {
     }
   }
 }
+
+// contact form https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
+
+function sendData(data) {
+  var XHR = new XMLHttpRequest();
+  var FD  = new FormData();
+
+  // Push our data into our FormData object
+  for(name in data) {
+    FD.append(name, data[name]);
+  }
+
+  // Define what happens on successful data submission
+  XHR.addEventListener('load', function(event) {
+    alert('Yeah! Data sent and response loaded.');
+  });
+
+  // Define what happens in case of error
+  XHR.addEventListener('error', function(event) {
+    alert('Oops! Something went wrong.');
+  });
+
+  // Set up our request
+  XHR.open('POST', 'mailTo:hoaplumrun@gmail.com');
+
+  // Send our FormData object; HTTP headers are set automatically
+  XHR.send(FD);
+}
